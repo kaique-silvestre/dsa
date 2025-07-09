aed31f66323f3b26c817ae50995a80e02cf56872
Kaique Gonzaga Silvestre <kaique.silvestre.22@gmail.com>
2025-07-08 22:21:36 -0300

    Refactor and clean up binary search implementation
    Removed the incorrect initial binary search implementation and refactored the second version to use a more standard approach with clearer variable names and logic. The new implementation returns the index of the found item or None if not found, and unnecessary comments and operations were removed for clarity.

--- 1-binary_search2.py
+++ 1-binary_search2.py
@@ -1,21 +1,22 @@
-# I have learned that the search should be performed by the array index instead of the value inside the index
-# When using the len function to get the last value of an array, we subtract its value by one, because the array starts at zero, and the len function stars counting on one
+def binary_search(arr: list, target: int | str) -> int:
+    low = 0
+    high = len(arr) - 1
 
-def pesquisa_binaria(lista, item):
-    cont = 0
-    menor = 0
-    maior = len(lista) - 1
-    while menor <= maior:
-        cont += 1
-        metade = (menor + maior) // 2
-        if lista[metade] == item:
-            return f"O item está na posição: {metade}, foram feitas {cont} operações"
-        if lista[metade] < item:
-            menor = metade + 1 # We are adding plus one because we already know that the midpoint isn't in the right item, otherwise it would not pass the first "if"
-        if lista[metade] > item:
-            maior = metade - 1 # Same logic as before, we already know that the midpoint isn't the searched item, and how we know the number is lower than the midpoint we subtract one
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] > target:
+            high = mid - 1
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            return mid 
     return None
 
-lista = range(1000000000000)
 
-print(pesquisa_binaria(lista, 1))        
+
+arr = range(100)
+
+print(binary_search(arr, 75))
+
+
+# I have used three "if" statament in the first time, there are unnecessary comparasions using three "if"s
